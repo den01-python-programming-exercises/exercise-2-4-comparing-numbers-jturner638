e3195d29f3b289ead78566bcd7dfa847630a7c74
Justin Turner <mrfathed@protonmail.com>
2021-12-05 07:37:57 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    number1 = int(input())
+    number2 = int(input())
+
+    if number1 > number2:
+        print("{} is greater than {}.".format(number1, number2))
+    elif number1 < number2:
+        print("{} is smaller than {}.".format(number1, number2))
+    else:
+        print("{} is equal to {}.".format(number1, number2))
 
 if __name__ == '__main__':
     main()
